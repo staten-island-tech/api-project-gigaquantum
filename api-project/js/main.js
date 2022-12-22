b753a651a906ef25f3e94fd4c62732228ab115ed
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-12-22 12:24:30 -0500

    Update main.js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,5 +1,9 @@
-// Create Search function for species
+// API: https://restcountries.com/#api-endpoints-v2-code
+// Image Color: https://www.npmjs.com/package/fast-average-color
 
 async function fetchAPI(url) {
-  apiData = await (await fetch(url)).json();
+  const apiData = await (await fetch(url)).json();
+  return apiData;
 }
+
+fetchAPI("https://restcountries.com/v2/all").then(console.log(apiData));
