23504a656043f30209aa59d6f0fd653b92487ace
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-12-22 19:20:57 -0500

    Update main.js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -2,8 +2,9 @@
 // Image Color: https://www.npmjs.com/package/fast-average-color
 
 async function fetchAPI(url) {
-  const apiData = await (await fetch(url)).json();
-  return apiData;
+  const apiData = await fetch(url);
+  const jsonData = await apiData.json();
+  console.log(jsonData);
 }
 
-fetchAPI("https://restcountries.com/v2/all").then(console.log(apiData));
+fetchAPI("https://restcountries.com/v2/all");
