39a0264628739974e70fa6e2b7fcc4f8e43272a7
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-12-24 00:21:44 -0500

    Update main.js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -2,9 +2,9 @@
 // Image Color: https://www.npmjs.com/package/fast-average-color
 
 import "../css/style.css";
-// import { getAverageColor } from "fast-average-color-node";
+import { getAverageColor } from "fast-average-color-node";
 
-function addCard(country, flagImg, demonym, color) {
+/* function addCard(country, flagImg, demonym, color) {
   document.getElementById("card-bin").insertAdjacentHTML(
     "beforeend",
     `<div class="card" style="background-color: ${color};">
@@ -15,23 +15,30 @@ function addCard(country, flagImg, demonym, color) {
   />
   <h2 class="card-header">${country}</h2>
 </div>`
-  ); //.style.backgroundColor = "red";
+  );
 }
 
 async function fetchAPI(url) {
-  const apiData = await fetch(url);
-  const jsonData = await apiData.json();
-  jsonData.forEach((obj) => {
-    addCard(obj.name, obj.flag, obj.demonym, "green");
-  });
+  try {
+    const apiData = await fetch(url);
+    const jsonData = await apiData.json();
+    return jsonData;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-fetchAPI("https://restcountries.com/v2/all");
-
-/*function displayAllCards(data) {
+function displayAllCards(data) {
   data.forEach((obj) => {
     addCard(obj.name, obj.flag, obj.demonym);
   });
 }
 
-// displayAllCards(apiData);*/
+displayAllCards(await fetchAPI("https://restcountries.com/v2/all"));
+*/
+
+getAverageColor(
+  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+).then((color) => {
+  console.log(color);
+});
