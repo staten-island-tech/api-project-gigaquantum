0d92f317cc3423769a20b9839a637bdff8a598b6
gigaquantum <113395660+gigaquantum@users.noreply.github.com>
2022-12-28 09:43:23 -0500

    commit

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -2,12 +2,12 @@
 // Image Color: https://www.npmjs.com/package/fast-average-color
 
 import "../css/style.css";
-import { getAverageColor } from "fast-average-color-node";
+// import { getAverageColor } from "fast-average-color-node";
 
-/* function addCard(country, flagImg, demonym, color) {
+function addCard(country, flagImg, demonym, color) {
   document.getElementById("card-bin").insertAdjacentHTML(
     "beforeend",
-    `<div class="card" style="background-color: ${color};">
+    `<div class="card" style="background-color: ${color}; filter: blur(8px);">
   <img
     class="card-img"
     src="${flagImg}"
@@ -34,11 +34,11 @@ function displayAllCards(data) {
   });
 }
 
-displayAllCards(await fetchAPI("https://restcountries.com/v2/all"));
-*/
+const countryData = await fetchAPI("https://restcountries.com/v2/all");
+displayAllCards(countryData);
 
-getAverageColor(
+/*getAverageColor(
   "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
 ).then((color) => {
   console.log(color);
-});
+});*/
